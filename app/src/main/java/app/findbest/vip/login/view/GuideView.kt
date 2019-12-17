4c6fef4f42bcefe1e6ada8b52ff55487d108af14
LIZHENCHUAN <395387944@qq.com>
2019-12-17 10:03:58 +0800

    添加校验token和获取token信息的方法

--- app/src/main/java/app/findbest/vip/login/view/GuideView.kt
+++ app/src/main/java/app/findbest/vip/login/view/GuideView.kt
@@ -3,11 +3,15 @@ package app.findbest.vip.login.view
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Base64
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 import app.findbest.vip.R
 import app.findbest.vip.login.api.LoginApi
+import app.findbest.vip.utils.CheckToken
 import app.findbest.vip.utils.RetrofitUtils
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import com.gyf.immersionbar.ImmersionBar
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.CoroutineStart
@@ -19,6 +23,9 @@ import org.jetbrains.anko.backgroundColor
 import org.jetbrains.anko.frameLayout
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
+import java.security.KeyFactory
+import java.security.interfaces.RSAPublicKey
+import java.security.spec.X509EncodedKeySpec
 
 class GuideView: AppCompatActivity() {
 
@@ -56,6 +63,13 @@ class GuideView: AppCompatActivity() {
                 .awaitSingle()
             if (it.code() in 200..299) {
                 println("token未过期")
+
+                val mPerferences: SharedPreferences =
+                    PreferenceManager.getDefaultSharedPreferences(this@GuideView)
+                val token = mPerferences.getString("token", "")
+                if (token != null) {
+                    isAccount(token)
+                }
             }
             if(it.code() == 401){
                 println("token过期，需要登录")
@@ -64,6 +78,36 @@ class GuideView: AppCompatActivity() {
             }
         } catch (throwable: Throwable) {
             println(throwable)
+        }
+    }
+
+    private fun isAccount(token: String){
+        val account = CheckToken(this@GuideView).jwtParse(token)
+        if(account == ""){
+            //token验证失败
+            return
+        }
+        //token验证成功获取到的status
+        when (account) {
+            AccountStatus.COMPLETED.arg -> {
+                //已完善信息
+            }
+            AccountStatus.UNCOMPLETED.arg -> {
+                //未完善信息
+
+            }
+            AccountStatus.PENDING.arg -> {
+                //审核中
+
+            }
+            AccountStatus.PASSED.arg -> {
+                //审核通过
+
+            }
+            else -> {
+                //审核失败
+
+            }
         }
     }
 }
--- app/src/main/java/app/findbest/vip/login/view/LoginActivity.kt
+++ app/src/main/java/app/findbest/vip/login/view/LoginActivity.kt
@@ -1,33 +1,32 @@
 package app.findbest.vip.login.view
 
 import android.annotation.SuppressLint
-import android.app.FragmentTransaction
 import android.content.Context
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Base64
 import android.view.Gravity
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceManager
 import app.findbest.vip.R
 import app.findbest.vip.commonfrgmant.BackgroundFragment
 import app.findbest.vip.commonfrgmant.ChooseCountry
 import app.findbest.vip.login.api.LoginApi
-import app.findbest.vip.register.api.RegisterApi
 import app.findbest.vip.register.view.RegisterActivity
 import app.findbest.vip.utils.BaseActivity
+import app.findbest.vip.utils.CheckToken
 import app.findbest.vip.utils.MimeType
 import app.findbest.vip.utils.RetrofitUtils
 import com.alibaba.fastjson.JSON
-import com.gyf.immersionbar.ImmersionBar
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Dispatchers
@@ -37,27 +36,11 @@ import kotlinx.coroutines.rx2.awaitSingle
 import okhttp3.RequestBody
 import org.jetbrains.anko.*
 import retrofit2.HttpException
+import java.security.KeyFactory
+import java.security.interfaces.RSAPublicKey
+import java.security.spec.X509EncodedKeySpec
 
-class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry.DialogSelect {
-    override fun clickAll() {
-        closeAlertDialog()
-    }
-
-    @SuppressLint("SetTextI18n")
-    override fun getSelectedItem(index: Int) {
-        when(index){
-            0 -> {
-                country.text = "+81"
-            }
-            1 -> {
-                country.text = "+86"
-            }
-            2 -> {
-                country.text = "+82"
-            }
-        }
-        closeAlertDialog()
-    }
+class LoginActivity : BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry.DialogSelect {
 
     private lateinit var phoneNumber: EditText
     private lateinit var country: TextView
@@ -89,11 +72,11 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
                             startActivity<RegisterActivity>()
                             overridePendingTransition(R.anim.right_in, R.anim.left_out)
                         }
-                    }.lparams(wrapContent, wrapContent){
+                    }.lparams(wrapContent, wrapContent) {
                         alignParentRight()
                         alignParentBottom()
                     }
-                }.lparams(matchParent,dip(75)){
+                }.lparams(matchParent, dip(75)) {
                     leftMargin = dip(15)
                     rightMargin = dip(10)
                 }
@@ -103,14 +86,14 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
                         text = "登录"
                         textColor = Color.parseColor("#FF333333")
                         textSize = 19f
-                    }.lparams(wrapContent, wrapContent){
+                    }.lparams(wrapContent, wrapContent) {
                         gravity = Gravity.CENTER_HORIZONTAL
                     }
                     linearLayout {
                         orientation = LinearLayout.HORIZONTAL
                         imageView {
                             imageResource = R.mipmap.login_ico_mobile
-                        }.lparams(dip(16), dip(20)){
+                        }.lparams(dip(16), dip(20)) {
                             gravity = Gravity.CENTER_VERTICAL
                         }
                         linearLayout {
@@ -126,7 +109,7 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
                                 }.lparams(dip(26), wrapContent)
                                 imageView {
                                     imageResource = R.mipmap.inverted_triangle
-                                }.lparams(dip(12),dip(7)){
+                                }.lparams(dip(12), dip(7)) {
                                     gravity = Gravity.CENTER_VERTICAL
                                     leftMargin = dip(8)
                                     rightMargin = dip(5)
@@ -142,36 +125,50 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
                                 hintTextColor = Color.parseColor("#FFD0D0D0")
                                 textSize = 15f
                                 singleLine = true
-                                addTextChangedListener(object: TextWatcher {
+                                addTextChangedListener(object : TextWatcher {
                                     override fun afterTextChanged(s: Editable?) {}
-                                    override fun beforeTextChanged(s: CharSequence?,start: Int,count: Int,after: Int ) {}
-                                    override fun onTextChanged(s: CharSequence?,start: Int,before: Int,count: Int) {
+                                    override fun beforeTextChanged(
+                                        s: CharSequence?,
+                                        start: Int,
+                                        count: Int,
+                                        after: Int
+                                    ) {
+                                    }
+
+                                    override fun onTextChanged(
+                                        s: CharSequence?,
+                                        start: Int,
+                                        before: Int,
+                                        count: Int
+                                    ) {
                                         if (s != null) {
-                                            if(s.isNotBlank()){
+                                            if (s.isNotBlank()) {
                                                 val pwd = pwd.text.toString()
-                                                if(pwd.isNotBlank()){
-                                                    button.backgroundResource = R.drawable.enable_around_button
+                                                if (pwd.isNotBlank()) {
+                                                    button.backgroundResource =
+                                                        R.drawable.enable_around_button
                                                 }
-                                            }else{
-                                                button.backgroundResource = R.drawable.disable_around_button
+                                            } else {
+                                                button.backgroundResource =
+                                                    R.drawable.disable_around_button
                                             }
                                         }
                                     }
                                 })
-                            }.lparams(matchParent, matchParent){
+                            }.lparams(matchParent, matchParent) {
                                 leftMargin = dip(5)
                             }
-                        }.lparams(matchParent, matchParent){
+                        }.lparams(matchParent, matchParent) {
                             leftMargin = dip(14)
                         }
-                    }.lparams(matchParent, dip(40)){
+                    }.lparams(matchParent, dip(40)) {
                         topMargin = dip(70)
                     }
                     linearLayout {
                         orientation = LinearLayout.HORIZONTAL
                         imageView {
                             imageResource = R.mipmap.login_ico_password_nor
-                        }.lparams(dip(16), dip(20)){
+                        }.lparams(dip(16), dip(20)) {
                             gravity = Gravity.CENTER_VERTICAL
                         }
                         linearLayout {
@@ -183,27 +180,41 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
                                 hintTextColor = Color.parseColor("#FFD0D0D0")
                                 textSize = 15f
                                 singleLine = true
-                                addTextChangedListener(object: TextWatcher {
+                                addTextChangedListener(object : TextWatcher {
                                     override fun afterTextChanged(s: Editable?) {}
-                                    override fun beforeTextChanged(s: CharSequence?,start: Int,count: Int,after: Int ) {}
-                                    override fun onTextChanged(s: CharSequence?,start: Int,before: Int,count: Int) {
+                                    override fun beforeTextChanged(
+                                        s: CharSequence?,
+                                        start: Int,
+                                        count: Int,
+                                        after: Int
+                                    ) {
+                                    }
+
+                                    override fun onTextChanged(
+                                        s: CharSequence?,
+                                        start: Int,
+                                        before: Int,
+                                        count: Int
+                                    ) {
                                         if (s != null) {
-                                            if(s.isNotBlank()){
+                                            if (s.isNotBlank()) {
                                                 val phone = phoneNumber.text.toString()
-                                                if(phone.isNotBlank()){
-                                                    button.backgroundResource = R.drawable.enable_around_button
+                                                if (phone.isNotBlank()) {
+                                                    button.backgroundResource =
+                                                        R.drawable.enable_around_button
                                                 }
-                                            }else{
-                                                button.backgroundResource = R.drawable.disable_around_button
+                                            } else {
+                                                button.backgroundResource =
+                                                    R.drawable.disable_around_button
                                             }
                                         }
                                     }
                                 })
                             }.lparams(matchParent, matchParent)
-                        }.lparams(matchParent, matchParent){
+                        }.lparams(matchParent, matchParent) {
                             leftMargin = dip(14)
                         }
-                    }.lparams(matchParent, dip(40)){
+                    }.lparams(matchParent, dip(40)) {
                         topMargin = dip(35)
                     }
                     textView {
@@ -215,7 +226,7 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
                             startActivity<ResetPassword>()
                             overridePendingTransition(R.anim.right_in, R.anim.left_out)
                         }
-                    }.lparams(wrapContent, wrapContent){
+                    }.lparams(wrapContent, wrapContent) {
                         gravity = Gravity.RIGHT
                         topMargin = dip(5)
                     }
@@ -229,15 +240,15 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
 
                             val phoneNum = phoneNumber.text.toString()
                             val pwd = pwd.text.toString()
-                            if(phoneNum.isNullOrBlank()){
+                            if (phoneNum.isNullOrBlank()) {
                                 toast("请输入手机号")
                                 return@setOnClickListener
                             }
-                            if(phoneNum.length !in 10..11){
+                            if (phoneNum.length !in 10..11) {
                                 toast("手机号格式不对")
                                 return@setOnClickListener
                             }
-                            if(pwd.isNullOrBlank()){
+                            if (pwd.isNullOrBlank()) {
                                 toast("请输入验证码")
                                 return@setOnClickListener
                             }
@@ -246,11 +257,11 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
                                 login()
                             }
                         }
-                    }.lparams(matchParent,dip(47)){
+                    }.lparams(matchParent, dip(47)) {
                         topMargin = dip(14)
                     }
-                }.lparams(matchParent, wrapContent){
-                    setMargins(dip(40),dip(40),dip(40),0)
+                }.lparams(matchParent, wrapContent) {
+                    setMargins(dip(40), dip(40), dip(40), 0)
                 }
                 setOnClickListener {
                     closeFocusjianpan()
@@ -259,14 +270,15 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
         }
     }
 
-    private suspend fun login(){
+    private suspend fun login() {
         try {
             val countryCode = country.text.toString().substring(1)
             val deviceType = "android"
             val platform = "mobile"
 
-            val mPerferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this@LoginActivity)
-            val deviceToken = mPerferences.getString("deviceToken","") as String
+            val mPerferences: SharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this@LoginActivity)
+            val deviceToken = mPerferences.getString("deviceToken", "") as String
 
             val params = mapOf(
                 "country" to countryCode,
@@ -291,17 +303,69 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
                 val mEditor = mPerferences.edit()
                 mEditor.putString("token", token)
                 mEditor.commit()
+
+                isAccount(token)
             }
-            if(it.code() == 500){
+            if (it.code() == 500) {
                 toast("密码错误")
             }
         } catch (throwable: Throwable) {
-            if(throwable is HttpException){
+            if (throwable is HttpException) {
                 println(throwable.message())
             }
         }
     }
 
+    private fun isAccount(token: String){
+        val account = CheckToken(this@LoginActivity).jwtParse(token)
+        if(account == ""){
+            //token验证失败
+            return
+        }
+        //token验证成功获取到的status
+        when (account) {
+            AccountStatus.COMPLETED.arg -> {
+                //已完善信息
+
+            }
+            AccountStatus.UNCOMPLETED.arg -> {
+                //未完善信息
+
+            }
+            AccountStatus.PENDING.arg -> {
+                //审核中
+
+            }
+            AccountStatus.PASSED.arg -> {
+                //审核通过
+
+            }
+            else -> {
+                //审核失败
+
+            }
+        }
+    }
+
+    override fun clickAll() {
+        closeAlertDialog()
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun getSelectedItem(index: Int) {
+        when (index) {
+            0 -> {
+                country.text = "+81"
+            }
+            1 -> {
+                country.text = "+86"
+            }
+            2 -> {
+                country.text = "+82"
+            }
+        }
+        closeAlertDialog()
+    }
 
     private fun openDialog() {
         val mTransaction = supportFragmentManager.beginTransaction()
@@ -349,4 +413,19 @@ class LoginActivity: BaseActivity(), BackgroundFragment.ClickBack, ChooseCountry
         phone.hideSoftInputFromWindow(phoneNumber.windowToken, 0)
         phone.hideSoftInputFromWindow(pwd.windowToken, 0)
     }
+}
+
+enum class AccountStatus(val arg: String){
+    /*
+         COMPLETED : 已完善信息
+         UNCOMPLETED : 未完善信息
+         PENDING : 审核中
+         PASSED : 审核通过
+         REJECT : 审核失败
+     */
+    COMPLETED("COMPLETED"),
+    UNCOMPLETED("UNCOMPLETED"),
+    PENDING("PENDING"),
+    PASSED("PASSED"),
+    REJECT("REJECT")
 }
